c80907cdfe9b74fe66e0ad92dd5fbd1e5ef8e0f3
Peughouia <peughouia.44@gmail.com>
2026-06-11 01:22:47 +0200

    init: initialisation du projet

---
+++ katabooks/src/main/java/com/nickel/katabooks/KatabooksApplication.java
@@ -0,0 +1,13 @@
+package com.nickel.katabooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatabooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KatabooksApplication.class, args);
+	}
+
+}
---
+++ katabooks/src/test/java/com/nickel/katabooks/KatabooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nickel.katabooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+//class KatabooksApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
